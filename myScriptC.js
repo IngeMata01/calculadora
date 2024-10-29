404e7ed24829c674797c8c363b67540256c354ca
IngeMata01 <22690475@tecvalles.mx>
2024-10-28 19:26:31 -0600

    Add files via upload

---
+++ myScriptC.js
@@ -0,0 +1,10 @@
+function resta() { 
+valor1= document.getElementsByName("num1")[0].value; 
+valor2= document.getElementsByName("num2")[0].value; res=parseInt(valor1)-parseInt(valor2); 
+alert("El resultado es "+res); 
+}; 
+function dividir() { 
+valor1= document.getElementsByName("num1")[0].value; 
+valor2= document.getElementsByName("num2")[0].value; div=parseInt(valor1)/parseInt(valor2); 
+alert("El resultado es "+div); 
+}; 
